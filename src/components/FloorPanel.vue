4cbd005c3b923dc8b2308e281664c5603aba0b24
Pavel Harelik <pasha.harelik@gmail.com>
2022-10-01 22:09:36 +0300

    Import ButtonCall component

--- src/components/FloorPanel.vue
+++ src/components/FloorPanel.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="floor-panel">
     <h3>{{ floorNumber }}</h3>
-    <button :disabled="isActive" @click="addCallOfElevator">Вызов</button>
+    <button-call :disabled="isActive" @click="addCallOfElevator" />
   </div>
 </template>
 
 <script>
+import ButtonCall from "@/components/UI/ButtonCall";
 export default {
   name: "FloorPanel",
+  components: { ButtonCall },
   props: {
     floorNumber: {
       type: Number,
@@ -30,10 +32,10 @@ export default {
 .floor-panel {
   flex-grow: 1;
 }
-button {
-  cursor: pointer;
-}
-button:disabled {
-  background-color: rgba(245, 109, 109, 0.6);
-}
+/*button {*/
+/*  cursor: pointer;*/
+/*}*/
+/*button:disabled {*/
+/*  background-color: rgba(245, 109, 109, 0.6);*/
+/*}*/
 </style>
